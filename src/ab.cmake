8803a78c72cabb8fdf439995d65b528f51f4ee23
Andrei Gramakov <mail@agramakov.me>
2024-04-17 20:27:18 +0200

    Add new public functions for working with non-standard projects (#2)
    Added functions:
    
    - `target_init_abcmake_custom(TARGET_NAME INCLUDE_DIR SOURCE_DIR)` - Initialize the target with abcmake with custom include and source directories
    - `target_abcmake_component (TARGETNAME COMPONENTPATH)` - Add a component to the target
    - `target_sources_directory(TARGETNAME SOURCE_DIR)` - Add all sources from the directory

--- src/ab.cmake
+++ src/ab.cmake
@@ -16,83 +16,81 @@
 # *************************************************************************
 
 set(ABCMAKE_VERSION_MAJOR 4)
-set(ABCMAKE_VERSION_MINOR 1)
+set(ABCMAKE_VERSION_MINOR 2)
 set(ABCMAKE_VERSION_PATCH 0)
 set(ABCMAKE_VERSION "${ABCMAKE_VERSION_MAJOR}.${ABCMAKE_VERSION_MINOR}.${ABCMAKE_VERSION_PATCH}")
 
+include(CMakeParseArguments)
 
 # *************************************************************************
 # Private functions
 # *************************************************************************
 
-function(_abc_AddProject Path OUT_ABCMAKE_VER)
-    if (EXISTS ${Path}/CMakeLists.txt)
-        message(DEBUG "Adding project ${Path}")
-        add_subdirectory(${Path})
+function(_abc_AddProject PATH OUT_ABCMAKE_VER)
+    if (EXISTS ${PATH}/CMakeLists.txt)
+        message(DEBUG "Adding project ${PATH}")
+        add_subdirectory(${PATH})
         
-        get_directory_property(version DIRECTORY ${Path} ABCMAKE_VERSION)
+        get_directory_property(version DIRECTORY ${PATH} ABCMAKE_VERSION)
         set(${OUT_ABCMAKE_VER} ${version} PARENT_SCOPE)
         if (NOT version)
-            message (STATUS "  ❌ ${child} is not a ABCMAKE project. Handle it manually.")
+            message (STATUS "  ❌ ${PATH} is not an ABCMAKE project. Handle it manually.")
         endif()
         
     else()
-        message (STATUS "  📁 ${child} is not a CMake project")
+        message (STATUS "  📁 ${PATH} is not a CMake project")
     endif()
 endfunction()
 
-# Add all projects from the lib subdirectory
-function(_abc_AddComponents TargetName)
+
+# Add all projects from the components subdirectory
+# @param TARGETNAME - name of the target to add components
+function(_abc_AddComponents TARGETNAME)
     # List of possible subprojects
     file(GLOB children RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/components ${CMAKE_CURRENT_SOURCE_DIR}/components/*)
     
-    # Link all subprojects to the ${TargetName}
+    # Link all subprojects to the ${TARGETNAME}
     foreach(child ${children})
         if(IS_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/components/${child})
-            message(DEBUG "${TargetName} found ${child}")
-            
-            _abc_AddProject(${CMAKE_CURRENT_SOURCE_DIR}/components/${child} ver)
-            
-            if (ver)
-                get_directory_property(to_link DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/components/${child} ABCMAKE_TARGETS)
-                message (STATUS "  ✅ Linking ${to_link} to ${TargetName}")
-                target_link_libraries(${TargetName} PRIVATE ${to_link})
-            endif()
-            
+            target_abcmake_component(${TARGETNAME} ${CMAKE_CURRENT_SOURCE_DIR}/components/${child})
         endif()
     endforeach()
     
 endfunction()
 
-# Add files from the ./src, ./include, ./lib to the project
-function(_abc_AddFiles TargetName)
-    file(GLOB_RECURSE SOURCES "src/*.cpp" "src/*.c")
-    message( DEBUG "${TargetName} sources: ${SOURCES}")
-    target_sources(${TargetName} PRIVATE ${SOURCES})
-    message( DEBUG "${TargetName} include: ${CMAKE_CURRENT_SOURCE_DIR}/include")
-    target_include_directories(${TargetName} PUBLIC include)
-endfunction()
 
-function(_abc_Install TargetName)
+# Install the target near the build directory
+# @param TARGETNAME - name of the target to install
+function(_target_install_near_build TARGETNAME)
     # install directory
-    # if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
-        set (CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/../install"
-        CACHE PATH "default install path" FORCE)
-    # endif()
-    install(TARGETS ${TargetName} DESTINATION ".")
+    set (CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/../install"
+         CACHE PATH "default install path" FORCE)
+    install(TARGETS ${TARGETNAME} DESTINATION ".")
 endfunction()
 
 # *************************************************************************
 # Public functions
 # *************************************************************************
 
+# Add all source files from the specified directory to the target
+# @param TARGETNAME - name of the target to add sources
+function(target_sources_directory TARGETNAME SOURCE_DIR)
+    file(GLOB_RECURSE SOURCES "${SOURCE_DIR}/*.cpp" "${SOURCE_DIR}/*.c")
+    message( DEBUG "${TARGETNAME} sources: ${SOURCES}")
+    target_sources(${TARGETNAME} PRIVATE ${SOURCES})
+endfunction()
+
+
 # Add to the project all files from ./src, ./include, ./lib
-function(target_init_abcmake TargetName)
+# @param TARGETNAME - name of the target to initialize
+# @param INCLUDE_DIR - path to the include directory
+# @param SOURCE_DIR - path to the source directory
+function(target_init_abcmake_custom TARGETNAME INCLUDE_DIR SOURCE_DIR)
 
     get_directory_property(hasParent PARENT_DIRECTORY)
     # if no parent, print the name of the target
     if (NOT hasParent)
-        message(STATUS "🔤 ${TargetName}")
+        message(STATUS "🔤 ${TARGETNAME}")
     endif ()
     
     # Report version
@@ -101,10 +99,30 @@ function(target_init_abcmake TargetName)
                  
     # Add target to the target list
     set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} APPEND PROPERTY
-                 ABCMAKE_TARGETS ${TargetName})
+                 ABCMAKE_TARGETS ${TARGETNAME})
         
-    _abc_AddFiles(${TargetName})
-    _abc_AddComponents(${TargetName})
-    _abc_Install(${TargetName})
+    target_sources_directory(${TARGETNAME} ${SOURCE_DIR})
+    target_include_directories(${TARGETNAME} PUBLIC ${INCLUDE_DIR})
+    _abc_AddComponents(${TARGETNAME})
+    _target_install_near_build(${TARGETNAME})
 
+endfunction()
+
+
+function(target_init_abcmake TARGETNAME)
+    target_init_abcmake_custom(${TARGETNAME} 
+                               ${CMAKE_CURRENT_SOURCE_DIR}/include 
+                               ${CMAKE_CURRENT_SOURCE_DIR}/src)
+endfunction()
+
+# Link the target to the component
+# @param TARGETNAME - name of the target for linking
+# @param COMPONENTPATH - path to the component to link
+function (target_abcmake_component TARGETNAME COMPONENTPATH)
+    _abc_AddProject(${COMPONENTPATH} ver)
+    if (ver)
+        get_directory_property(to_link DIRECTORY ${COMPONENTPATH} ABCMAKE_TARGETS)
+        message (STATUS "  ✅ Linking ${to_link} to ${TARGETNAME}")
+        target_link_libraries(${TARGETNAME} PRIVATE ${to_link})
+    endif()
 endfunction()
--- tests/project_custom/run.ps1
+++ tests/project_custom/run.ps1
@@ -5,7 +5,7 @@ pushd $PSScriptRoot
 $git_root = (git rev-parse --show-toplevel)
 $env:ABCMAKE_PATH = "$git_root/src"
 
-echo "🚀 Building project"
+echo "🚀 Building project_custom"
 
 cmake -B build -DCMAKE_BUILD_TYPE=Release -G "Ninja"
 cmake --build build --config Release
