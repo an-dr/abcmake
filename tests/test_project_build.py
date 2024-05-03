4729b4eecf30cc18767838bce0d6235c07c32d13
Andrei Gramakov <mail@agramakov.me>
2024-05-03 23:29:59 +0200

    Implement python unit tests (#11)

---
+++ tests/test_project_build.py
@@ -0,0 +1,25 @@
+import subprocess
+import unittest
+import os
+from tools import TestCMake
+
+class TestBuild(TestCMake):
+    
+    def test_build_interdep(self):
+        self.build_cmake("test_interdep")
+    
+    def test_default_project(self):
+        self.build_cmake("test_default_project")
+    
+    def test_project_custom(self):
+        self.build_cmake("test_project_custom")
+    
+    def test_compile_commands(self):
+        self.build_cmake("test_compile_commands")
+        # verify file exists
+        self.assertTrue(os.path.exists("test_compile_commands/build/compile_commands.json"),
+                        "compile_commands.json does not exist")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/tools.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# *************************************************************************
+#
+# Copyright (c) 2024 Andrei Gramakov. All rights reserved.
+#
+# site:    https://agramakov.me
+# e-mail:  mail@agramakov.me
+#
+# *************************************************************************
+import os
+import subprocess
+import unittest
+import shutil
+
+class TestCMake(unittest.TestCase):
+    def setUp(self) -> None:
+        self.ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+        os.environ["ABCMAKE_PATH"] = self.ROOT_PATH + "/../src"
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        return super().tearDown()
+    
+    def build_cmake(self, dir):
+        os.chdir(f"{self.ROOT_PATH}/{dir}")
+        
+        # cleanup
+        shutil.rmtree("build", ignore_errors=True)
+        shutil.rmtree("install", ignore_errors=True)
+        
+        p = subprocess.run(['cmake', '-B', 'build', '-DCMAKE_BUILD_TYPE=Release'])
+        self.assertEqual(p.returncode, 0, p.stdout)
+        p = subprocess.run(['cmake', '--build', 'build', '--config', 'Release'])
+        self.assertEqual(p.returncode, 0, p.stdout)
+        p = subprocess.run(['cmake', '--install', 'build', '--config', 'Release'])
+        self.assertEqual(p.returncode, 0, p.stdout)
+        os.chdir(self.ROOT_PATH)
