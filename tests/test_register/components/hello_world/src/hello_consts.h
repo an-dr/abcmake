b4561a40d718a269d0ffaaa3e0b6be556ceafe13
Andrei Gramakov <mail@agramakov.me>
2024-05-09 12:52:43 +0200

    Add a test

---
+++ tests/test_register/components/hello_world/src/hello_consts.h
@@ -0,0 +1,22 @@
+// *************************************************************************
+//
+// Copyright (c) 2024 Andrei Gramakov. All rights reserved.
+//
+// site:    https://agramakov.me
+// e-mail:  mail@agramakov.me
+//
+// *************************************************************************
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define HELLO_STR "Hello\n"
+
+
+#ifdef __cplusplus
+}
+#endif
